d3abd1442aa633364dca8727000be2d8d5b30346
DrowsySeal <drowsyseal@gmail.com>
2024-01-28 14:06:18 -0600

    Nav fixes

--- js/script.js
+++ js/script.js
@@ -4,16 +4,36 @@ const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
+document.addEventListener('touchstart', function() {});
+
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+    closeIcon.style.visibility = "hidden";
+    menuIcon.style.visibility = "visible";
   } else {
     menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    closeIcon.style.visibility = "visible";
+    menuIcon.style.visibility = "hidden";
   }
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+//////////////// DROPDOWN ////////////////
+const dropdown = document.querySelector(".dropdown");
+const dropdownOptions = document.querySelector(".dropdown-options");
+
+document.addEventListener("click", function(event) {
+  var navActive = dropdown.contains(event.target);
+  if (dropdown.classList.contains("showDropdown")) {
+    dropdown.classList.remove("showDropdown");
+    dropdownOptions.style.display = "none";
+  } else if (navActive) {
+    dropdown.classList.add("showDropdown");
+    dropdownOptions.style.display = "block";
+  } else {
+    dropdown.classList.remove("showDropdown");
+    dropdownOptions.style.display = "none";
+  }
+});
